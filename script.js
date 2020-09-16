491ad0f1bd460b9d30fd1f54a43527fee3f173b6
Maks Kosar <makskosar79@gmail.com>
2020-09-16 18:10:39 +0300

    done wiyh homework-2

---
+++ script.js
@@ -0,0 +1,177 @@
+// let uah = +prompt('how much money do you have?');
+// let usd = 28.06;
+// let totalUahInUsd = uah / usd + '  USD';
+// if (uah == '' || uah == undefined) {
+//     alert('write something!!!!')
+// } else { 
+//     alert(totalUahInUsd)
+// }
+
+
+// let eur = 33.11;
+// let totalUahInEur = uah / eur + '   EUR';
+// if (uah == '' || uah == undefined) {
+//     alert('write something!!!!')
+// } else { 
+//     alert(totalUahInEur)
+// }
+
+
+// let oil = 39.65;
+// let totalOilInUah = (uah / usd) / oil + 'Oil bbl';
+// if (uah == '' || uah == undefined) {
+//     alert('write something!!!!')
+// } else { 
+//     alert(totalOilInUah)
+// }
+
+// let gold = 1730;
+// let totalGoldInUah = uah / gold + 'gram';
+// if (uah == '' || uah == undefined) {
+//     alert('write something!!!!')
+// } else { 
+//     alert(totalGoldInUah)
+// }
+
+
+
+
+
+
+
+// let sum = prompt('sum your order please')
+
+// if (sum <= 500) {
+//     sum = sum - (sum * 0.01);
+// } else if (sum > 500 && sum <= 1000 ) {
+//     sum = sum -(sum * 0.05);
+// } else if (sum > 1000) {
+//     sum = sum - (sum * 0.1);
+//     alert('congratulations you received gift certificate for 200 hryvnias')
+// } 
+
+// alert(sum);
+
+
+
+
+
+
+// let questionOne = prompt('рік незалежності України?');
+//     if(questionOne == 1991) {
+//         questionOne = 1;
+//     } else {
+//         questionOne = 0;
+//     }
+
+
+// let questionTwo = prompt('як зали найвідомішого цезаря Римської Імперії?');
+//     if (questionTwo.toLowerCase() == "юлій") {
+//         questionTwo = 1;
+//     } else {
+//         questionTwo = 0;
+//     }
+
+// let questionThree = prompt('Найвища гора світу?');
+//     if(questionThree.toUpperCase() == "ЕВЕРЕСТ" || questionThree.toUpperCase() == "ДЖАМАЛУНГМА"){
+//         questionThree = 1;
+//     } else {
+//         questionThree = 0;
+//     }
+
+// let questionFour = prompt('Найнижча точка світу ?');
+//     if(questionFour.toLowerCase() == "маріанська впадина") {
+//         questionFour = 1;
+//     } else {
+//         questionFour = 0;
+//     }
+
+// let questionFive = prompt("головні антогоністи всесвіту 'STAR WARS' ?");
+//     if(questionFive.toUpperCase() == 'СІТХИ') {
+//         questionFive = 1;
+//     } else {
+//         questionFive = 0;
+//     }
+
+// let totalScore = parseInt(questionFive + questionFour + questionThree + questionTwo + questionOne);
+//     if(totalScore < 5) {
+//         totalScore = 'ваш бал' + totalScore  
+//         alert(totalScore);
+//     } else if(totalScore = 5) {
+//         totalScore = 'вітаю ваш бал' + totalScore + 1 + 'очок . Та перші 4 питання очевидні , але завдяки 5 питанню я знаю що ви дивились "STAR WARS" ';
+//         alert(totalScore);
+//     };
+
+
+
+
+
+
+
+// let number = +prompt('Введіть трьохзначне число');
+// if (number && isNaN(number) == false && number.toString().length == 3) {
+//     number = number.toString();
+//     let arrNumbers = number.split('');
+//     if (arrNumbers[0] == arrNumbers[1] || arrNumbers[0] == arrNumbers[2] || arrNumbers[1] == arrNumbers[2]) {
+//         alert(`в числі -${arrNumbers} є однакові цифри!`);
+//     } else {
+//         alert('цифри не співпали')
+//     }
+//     } else {
+//         alert('ви не дотримались умови');
+// };
+
+
+
+
+
+
+
+// let button = prompt("Вибери кнопку від 1 до = на клавіатурі ");
+// switch (button) {
+//   case "1":
+//     alert("+ Shift = !");
+//     break;
+//   case "2":
+//     alert("+ Shift = @ ");
+//     break;
+//   case "3":
+//     alert("S+ Shift = #");
+//     break;
+//   case "4":
+//     alert("+ Shift = $");
+//     break;
+//   case "5":
+//     alert("+ Shift =  ");
+//     break;
+//   case "6":
+//     alert("+ Shift = ^ ");
+//     break;
+//   case "7":
+//     alert("+ Shift = & ");
+//     break;
+//   case "8":
+//     alert("+ Shift = *");
+//     break;
+//   case "9":
+//     alert("+ Shift =  (");
+//     break;
+//   case "0":
+//     alert("+ Shift =  ) ");
+//     break;
+//   case "-":
+//     alert("+ Shift =  _ ");
+//     break;
+//   case "=":
+//     alert("+ Shift = +");
+//     break;
+//   default:
+//     alert("ви  натиснули на невірну кнопку");
+// };
+
+
+
+
+
+//юліан по поводу макету дороблю його з пятниці на суботу з часом проблеми + нехочу відстати по дж тому надіюсь на розуміння 
+// і те шо ти це прочиташ ))))))
